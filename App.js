f7e29cd457aceb1c74bb462e36218982544ba804
viral <viral@viraldevmurari.com>
2024-02-11 16:24:54 +0530

    Episode1

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world From React!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(heading); //object
+
+const parent = React.createElement("div", { id: "Parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h1", {}, "I am a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h1", {}, "I am a h2 tag"),
+  ]),
+]);
+root.render(parent);
