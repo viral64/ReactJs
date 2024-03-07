480f30189fe322413c2af75a7933767504a45a74
viral <viral@viraldevmurari.com>
2024-03-08 00:21:19 +0530

    1:32

---
+++ src/Components/RestaurantMenu.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.3038945&lng=70.80215989999999&restaurantId=685351&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const json = await data.json();
+    console.log(json);
+    setResInfo(json.data);
+  };
+
+  return resInfo === null  ? (
+    <Shimmer />
+  ) : (
+    <div className="menu">
+      <h1>Name of Restuarant</h1>
+      <h2>Menu</h2>
+      <ul>
+        <li>Biryani</li>
+        <li>Burgers</li>
+        <li>Diet Coke</li>
+      </ul>
+    </div>
+  );
+};
+export default RestaurantMenu;
